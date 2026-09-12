d8b9df764743f71903e89e836ac908bfc8ae4296
szenglein <13049685+SZenglein@users.noreply.github.com>
2026-09-12 20:24:17 +0200

    Add demo how to use the library and a simple web UI

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -18,7 +18,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let api = api_router(backend);
 
     // Nest the API under /api
-    let app = Router::new().nest("/api", api);
+    let app = Router::new()
+        .nest("/api", api)
+        .fallback_service(ServeDir::new("demo/static"));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
     println!("Server running on http://{}:", addr);
